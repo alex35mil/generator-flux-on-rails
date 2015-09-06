ad612b3a5f6a38788406d032329d333dfc429a6c
Alex Fedoseev <alex.fedoseev@gmail.com>
2015-09-06 23:08:55 +0300

    Disable sourcemaps in production

--- app/templates/app/server.app.js
+++ app/templates/app/server.app.js
@@ -1,8 +1,16 @@
-/* eslint no-var: 0 */
+/* eslint no-var: 0, no-process-env: 0 */
 
-require('babel/register')({
-  sourceMap: 'inline'
-});
+var babelRegister = require('babel/register');
+
+var isProduction  = process.env.NODE_ENV === 'production';
+
+if (isProduction) {
+  babelRegister();
+} else {
+  babelRegister({
+    sourceMap: 'inline'
+  });
+}
 
 var initter = require('./app/bundles/app/initters/server'),
     config  = require('./config/server.app');
